0abbb841b45976b8fd544738874e6d357528909a
Puja Kumari Raut <pujarau@cybage.com>
2020-12-29 16:06:18 +0530

    first commit

---
+++ js/newRegister.js
@@ -0,0 +1,48 @@
+
+//Functions for registrations of upcoming courses
+
+function register(){
+	document.getElementById("register").innerHTML = "Successfully Registered!";
+	document.getElementById("register").style.color="black";
+	document.getElementById("register").disabled = true;
+     document.getElementById("register").style.backgroundColor = "green";
+ 
+
+	
+	
+}
+
+function register1(){
+	document.getElementById("register1").innerHTML = "Successfully Registered!";
+	document.getElementById("register1").style.color="black";
+	document.getElementById("register1").disabled = true;
+	 document.getElementById("register1").style.backgroundColor = "green";
+}
+
+function register2(){
+	document.getElementById("register2").innerHTML = "Successfully Registered!";
+	document.getElementById("register2").style.color="black";
+	document.getElementById("register2").disabled = true;
+	 document.getElementById("register2").style.backgroundColor = "green";
+}
+
+function register3(){
+	document.getElementById("register3").innerHTML = "Successfully Registered!";
+	document.getElementById("register3").style.color="black";
+	document.getElementById("register3").disabled = true;
+	 document.getElementById("register3").style.backgroundColor = "green";
+}
+
+function register4(){
+	document.getElementById("register4").innerHTML = "Successfully Registered!";
+	document.getElementById("register4").style.color="black";
+	document.getElementById("register4").disabled = true;
+	 document.getElementById("register4").style.backgroundColor = "green";
+}
+
+function register5(){
+	document.getElementById("register5").innerHTML = "Successfully Registered!";
+	document.getElementById("register5").style.color="black";
+	document.getElementById("register5").disabled = true;
+	 document.getElementById("register5").style.backgroundColor = "green";
+}
